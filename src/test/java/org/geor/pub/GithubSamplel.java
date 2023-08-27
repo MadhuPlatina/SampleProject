afa6f0cb5e512d9e1d43b26ec8d636c0d8d87f1d
Venkata Bachu <venkata.bachu@gmail.com>
2023-08-27 14:42:14 -0400

    Changed URL from amazon to Google

--- src/test/java/org/geor/pub/GithubSamplel.java
+++ src/test/java/org/geor/pub/GithubSamplel.java
@@ -1,5 +1,7 @@
 package org.geor.pub;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -13,8 +15,17 @@ public class GithubSamplel {
 	public static void main(String[] args) {
 		WebDriverManager.chromedriver().setup();
 		 driver=new ChromeDriver();
-		 driver.get("https://www.amazon.com/");
-		 driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("kids dresses", Keys.ENTER);
+		 driver.get("https://www.google.com/");
+		 driver.findElement(By.id("APjFqb")).sendKeys("kids dresses", Keys.ENTER);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		 //driver.findElement(null)
+		 
+		 
+		 
+		 
+		 
+		 
+		// driver.close();
 		 
 
 	}
