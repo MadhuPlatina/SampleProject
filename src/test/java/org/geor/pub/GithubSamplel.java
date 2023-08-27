b5cc51573733a72edc149b4f0fc55683abc33775
Venkata Bachu <venkata.bachu@gmail.com>
2023-08-27 15:11:27 -0400

    changed Url from Walmart to FoodDepot and did some actions

--- src/test/java/org/geor/pub/GithubSamplel.java
+++ src/test/java/org/geor/pub/GithubSamplel.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -15,11 +16,14 @@ public class GithubSamplel {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		 driver=new ChromeDriver();
-		 driver.get("https://www.walmart.com/");
+		 driver.get("https://www.fooddepot.com/");
+		 String Title=driver.getTitle();
 		 driver.manage().window().maximize();
-		 driver.findElement(By.xpath("//*[text()='Departments']")).click();
+		 driver.findElement(By.xpath("//ul[@id='top-menu']//a[text()='Weekly Ad']")).click();
 		 Thread.sleep(10);
-		 driver.findElement(By.xpath("//button[text()='Back to School']")).click();
+		 driver.findElement(By.xpath("//img[@src='/wp-content/uploads/2020/07/fooddepot-logo.png']")).click();
+		 String ExpectedTitle=driver.getTitle();
+		 Assert.assertEquals(Title, ExpectedTitle);
 		 
 			 
 		 
