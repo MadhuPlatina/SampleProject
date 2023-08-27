9cfa4b4c87fad526a7427e663afb4e9f02b9cdd4
Venkata Bachu <venkata.bachu@gmail.com>
2023-08-27 14:53:50 -0400

    Changed to google url to Walmart

--- src/test/java/org/geor/pub/GithubSamplel.java
+++ src/test/java/org/geor/pub/GithubSamplel.java
@@ -12,18 +12,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class GithubSamplel {
 
 	public static WebDriver driver;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		 driver=new ChromeDriver();
-		 driver.get("https://www.google.com/");
-		 driver.findElement(By.id("APjFqb")).sendKeys("kids dresses", Keys.ENTER);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		 //driver.findElement(null)
+		 driver.get("https://www.walmart.com/");
+		 driver.manage().window().maximize();
+		 driver.findElement(By.xpath("//*[text()='Departments']")).click();
+		 Thread.sleep(10);
+		 driver.findElement(By.xpath("//button[text()='Back to School']")).click();
 		 
-		 
-		 
-		 
-		 
+			 
 		 
 		// driver.close();
 		 
